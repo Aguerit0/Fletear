ecd24cd0a4518514aac1d6af627a22dc66b4e646
Esteban Agüero <cuentauniversidading@gmail.com>
2023-01-19 14:27:56 -0300

    editar perfil contraseña andando

--- php/template/adminNav.php
+++ php/template/adminNav.php
@@ -31,91 +31,11 @@
         </ul>
       </li><!-- End Icons Nav -->
 
-      <li class="nav-item">
-        <a class="nav-link collapsed" data-bs-target="#components-nav" data-bs-toggle="collapse" href="#">
-          <i class="bi bi-menu-button-wide"></i><span>Novedades de Guardia</span><i class="bi bi-chevron-down ms-auto"></i>
-        </a>
-        <ul id="components-nav" class="nav-content collapse " data-bs-parent="#sidebar-nav">
-         
-          <li>
-            <a href="novedades-agregar.php">
-              <i class="bi bi-circle"></i><span>Agregar registros</span>
-            </a>
-          </li>
-          <li>
-            <a href="novedades-tabla.php">
-              <i class="bi bi-circle"></i><span>Ver registros</span>
-            </a>
-          </li>
-        </ul>
-      </li><!-- End Components Nav -->
 
-      <li class="nav-item">
-        <a class="nav-link collapsed" data-bs-target="#forms-nav" data-bs-toggle="collapse" href="#">
-          <i class="bi bi-journal-text"></i><span>Novedades de Relevancia</span><i class="bi bi-chevron-down ms-auto"></i>
-        </a>
-        <ul id="forms-nav" class="nav-content collapse " data-bs-parent="#sidebar-nav">
-      
-          <li>
-            <a href="novedades-relevancia-agregar.php">
-              <i class="bi bi-circle"></i><span>Agregar registros</span>
-            </a>
-          </li>
-          <li>
-            <a href="novedades-relevancia-tabla.php">
-              <i class="bi bi-circle"></i><span>Ver registros</span>
-            </a>
-          </li>
-          <li>
-            <a href="novedades-ver-mapa.php">
-              <i class="bi bi-circle"></i><span>Ver Mapa</span>
-            </a>
-          </li>
-        </ul>
-      </li><!-- End Forms Nav -->
-
-      <li class="nav-item">
-        <a class="nav-link collapsed" data-bs-target="#tables-nav" data-bs-toggle="collapse" href="#">
-          <i class="bi bi-layout-text-window-reverse"></i><span>Ingreso de Personas</span><i class="bi bi-chevron-down ms-auto"></i>
-        </a>
-        <ul id="tables-nav" class="nav-content collapse " data-bs-parent="#sidebar-nav">
-         
-          <li>
-            <a href="ingreso-personas-agregar.php">
-              <i class="bi bi-circle"></i><span>Agregar persona</span>
-            </a>
-          </li>
-          <li>
-            <a href="ingreso-personas-tabla.php">
-              <i class="bi bi-circle"></i><span>Ver personas</span>
-            </a>
-          </li>
-        </ul>
-      </li><!-- End Tables Nav -->
-
-      <li class="nav-item">
-        <a class="nav-link collapsed" data-bs-target="#charts-nav" data-bs-toggle="collapse" href="#">
-          <i class="bi bi-bar-chart"></i><span>Registro Secuestros</span><i class="bi bi-chevron-down ms-auto"></i>
-        </a>
-        <ul id="charts-nav" class="nav-content collapse " data-bs-parent="#sidebar-nav">
-        
-         
-          <li>
-            <a href="registro-secuestros-agregar.php">
-              <i class="bi bi-circle"></i><span>Agregar registros</span>
-            </a>
-          </li>
-          <li>
-          <a href="registro-secuestros-tabla.php">
-              <i class="bi bi-circle"></i><span>Ver registros</span>
-            </a>
-          </li>
-        </ul>
-      </li><!-- End Charts Nav -->
       <li class="nav-heading">Paginas</li>
 
       <li class="nav-item">
-        <a class="nav-link collapsed" href="usuarios-perfil.php">
+        <a class="nav-link collapsed" href="perfil.php">
           <i class="bi bi-person"></i>
           <span>Perfil</span>
         </a>
